e3e7c86bf1e2369228468a2b3361b9ec400ae09f
Chris Matthias <chrism@shorelinemedianj.com>
2020-03-16 14:46:20 -0400

    updating github repo location in plugin

--- sl9-covid-19.php
+++ sl9-covid-19.php
@@ -1,15 +1,15 @@
 <?php
 /**
 * Plugin Name:          Shoreline COVID 19
-* Plugin URI:           https://github.com/shorelinemedia/sl9-covid-19-banner
+* Plugin URI:           https://github.com/shorelinemedia/sl9-covid-19
 * Description:          Add a banner to a WP Multisite indicating availability of COVID 19 test kits
-* Version:              1.0.1
+* Version:              1.0.2
 * Author:               Shoreline Media
 * Author URI:           https://shoreline.media
 * License:              GNU General Public License v2
 * License URI:          http://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain:          sl9-covid-19
-* GitHub Plugin URI:    https://github.com/shorelinemedia/sl9-covid-19-banner
+* GitHub Plugin URI:    https://github.com/shorelinemedia/sl9-covid-19
 */
 
 // Customizer scoped to 'editor' user role to set true/false about test kit availability
